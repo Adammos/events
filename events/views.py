6dca1bf9f9281711b5415becde134972bd319f99
Adam Janca <janca.adam89@gmail.com>
2018-10-29 22:52:04 +0100

    models_updated

--- events/views.py
+++ events/views.py
@@ -1,22 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Event 
+
 def index(request):
-	html = '''
-	<h1>Hey Client, my app is running!</h1>
-	<p>Check out our <a href="/events">offerings</a></p>
-	'''
-	return HttpResponse(html)
+	return render(request, 'events/index.html')
+
 
 def event_listing(request):
-	html = '''
-	<ul>
-		<li>Chill on the beach<a href="/event/Chill">detail</a></li>
-		<li>Camping in the woods <a href="/event/Camping">detail</a></li>
-		<li>Flying into the space <a href="/event/Flying">detail</a></li>
-	</ul>
-	'''
-	return HttpResponse(html)
+	events = Event.objects.all()
+	return render(request, 'events/event_listing.html', {'events': events}) 
 
 def event_detail(request, name):
 	data = {'Chill': '<h2>Chill on the beach for just $400</h2>',
